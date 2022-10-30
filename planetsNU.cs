3a22dd686b96f39d07db3c8b662fa6cafe46ade6
JU1111111 <julian.spiegel.hm@gmail.com>
2022-10-30 23:01:19 +0100

    yeet

---
+++ planetsNU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumSharp;
+
+namespace planetSim
+{
+    internal class planetsNU
+    {
+        public double mass;
+        public NumSharp.NDArray pos;
+        public NumSharp.NDArray vel;
+        public NumSharp.NDArray[]? forcelist;
+        public NumSharp.NDArray[]? force;
+
+        public planetsNU(double m, NumSharp.NDArray position, NDArray vel)
+        {
+            this.mass = m;
+            this.pos = position;
+            this.vel = vel;
+
+        }
+    }
+}
