745094c7a2bd1e6fa9554fe76a63f334379f83a2
JU1111111 <julian.spiegel.hm@gmail.com>
2022-10-31 21:04:53 +0100

    speeed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using planetSim;
+using NumSharp;
 
+
+
+var pos1 = np.array(new double[] {1.0, 0.0});
+var vel1 = np.array(new double[] { 0.0, 0.1 });
+
+Planet pl1 = new(100000.0, pos1, vel1);
+
+
+var pos2 = np.array(new double[] { 0.0, 0.0 });
+var vel2 = np.array(new double[] { 0.0, 0.0 });
+
+Planet pl2 = new(999999999.0, pos2, vel2);
+
+
+
+Planet[] pls = { pl1, pl2 };
+
+Universe universe = new(pls);
+universe.AddPlanetIds();
+
+double minT = 0, maxT = 100, dT = 0.0001;
+
+universe.SimThisshit(minT, maxT, dT);
+
+
+/* foreach (var bruh in universe.planets[0].xPoslist)
+{
+    Console.WriteLine(bruh);
+}*/
--- planetsNU.cs
+++ planetsNU.cs
@@ -1,26 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NumSharp;
+﻿using NumSharp;
+
 
 namespace planetSim
 {
-    internal class planetsNU
+    internal class Planet
     {
         public double mass;
         public NumSharp.NDArray pos;
         public NumSharp.NDArray vel;
-        public NumSharp.NDArray[]? forcelist;
-        public NumSharp.NDArray[]? force;
+        public NumSharp.NDArray force = 0;
+        public List<NumSharp.NDArray> forcelist = new();
+        public int? iD;
+        public List<Double> xPoslist = new();
+        public List<Double> yPoslist = new();
 
-        public planetsNU(double m, NumSharp.NDArray position, NDArray vel)
+        public Planet(double m, NumSharp.NDArray position, NDArray vel)
         {
             this.mass = m;
             this.pos = position;
             this.vel = vel;
-
         }
+
+        public void CalcFTo(Planet pl)
+        {
+            double gamma = 6.673 * Math.Pow(10, -11);
+            NumSharp.NDArray rvec = this.pos - pl.pos;
+            NumSharp.NDArray rsqt = NumSharp.np.power(rvec, 2);
+            NumSharp.NDArray rVal = rsqt[0] + rsqt[1];
+            //double rVal = np.linalg.norm(rvec);
+            NDArray Fg = gamma * (pl.mass * this.mass / Math.Pow(rVal, 3)) * rvec;
+            this.forcelist.Add(Fg);
+            pl.forcelist.Add(-1 * Fg);
+        }
+
+        public void CalFToListAndAdd(Planet[] plList)
+        {
+            for (int i = 0; i < plList.Length; i++)
+            {
+                Planet pl = plList[i];
+                if (pl.iD != this.iD)
+                {
+                    CalcFTo(pl);
+                }
+            }
+            foreach (NumSharp.NDArray f in this.forcelist)
+            {
+                force += f;
+            }
+        }
+
+        public void CalcPosAndVel(double dT)
+        {
+            NumSharp.NDArray a = this.force / this.mass;
+            NumSharp.NDArray newPos = 1 / 2 * a * Math.Pow(dT, 2) + this.vel * dT + this.pos;
+            NumSharp.NDArray newVel = a * dT + this.vel;
+
+            this.pos = newPos;
+            this.vel = newVel;
+            this.xPoslist.Add(newPos[0]);
+            this.yPoslist.Add(newPos[1]);
+        }
+    }
+    internal class Universe
+    {
+        public Planet[] planets;
+
+        public Universe(Planet[] planets)
+        {
+            this.planets = planets;
+        }
+
+        public bool CalcFAndSum()
+        {
+            for (int i = 0; i < this.planets.Length / 2; i++)
+            {
+                Planet pl = this.planets[i];
+                pl.CalFToListAndAdd(this.planets);
+            }
+            return true;
+        }
+
+        public void AddPlanetIds()
+        {
+            for (int i = 0; i < this.planets.Length; i++)
+            {
+                Planet pl = planets[i];
+                pl.iD = i;
+            }
+        }
+
+        public void SimThisshit(double minT, double maxT, double dT)
+        {
+            DateTime start = DateTime.UtcNow;
+            int steps = Convert.ToInt32(maxT / dT);
+            for (int i = 0; i < steps; i++)
+            {
+                bool fs = this.CalcFAndSum();
+
+                if (fs)
+                {
+                    foreach (var pl in this.planets)
+                    {
+                        pl.CalcPosAndVel(dT);
+                        pl.forcelist.Clear();
+                    }
+                }
+                /*if(i%1000 == 0)
+                {
+                    Console.WriteLine(i);
+                }*/
+            }
+            DateTime end = DateTime.UtcNow;
+            TimeSpan timeDiff = end - start;
+            Console.WriteLine(Convert.ToInt32(timeDiff.TotalMilliseconds));
+        }
+
     }
 }
