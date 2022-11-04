d2e76cb90faed7dc6b8993fb802a473d275a7d02
JU1111111 <julian.spiegel.hm@gmail.com>
2022-11-04 22:18:57 +0100

    dsafdg

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using planetSim;
+﻿using planetSim;
 using NumSharp;
+using ScottPlot;
+
+/*Class1 yee = new("yeet");
+
+yee.bruh();*/
+
+double[] pos1 = new double[] {1.0, 0.0};
+double[] vel1 = new double[] { 0.0, 0.1 };
+
+Planet pl1 = new(10000.0, pos1, vel1);
 
 
-
-var pos1 = np.array(new double[] {1.0, 0.0});
-var vel1 = np.array(new double[] { 0.0, 0.1 });
-
-Planet pl1 = new(100000.0, pos1, vel1);
-
-
-var pos2 = np.array(new double[] { 0.0, 0.0 });
-var vel2 = np.array(new double[] { 0.0, 0.0 });
+double[] pos2 = new double[] { 0.0, 0.0 };
+double[] vel2 = new double[] { 0.0, 0.0 };
 
 Planet pl2 = new(999999999.0, pos2, vel2);
 
@@ -25,6 +27,24 @@ universe.AddPlanetIds();
 double minT = 0, maxT = 100, dT = 0.0001;
 
 universe.SimThisshit(minT, maxT, dT);
+
+var plt = new ScottPlot.Plot(600, 400);
+plt.Benchmark(enable: true);
+plt.Style(Style.Black);
+plt.Title("Style.Black");
+plt.XLabel("t");
+plt.YLabel("Cordinate");
+
+List <Double> pl1xcordL = universe.planets[0].xPoslist;
+List<Double> pl1ycord = universe.planets[0].yPoslist;
+double[] xCordpl1 = pl1xcordL.ToArray();
+double[] yCordpl1 = pl1ycord.ToArray();
+
+int sampleRate = 10000;
+
+plt.AddSignal(xCordpl1, sampleRate, label:"Xcord");
+plt.AddSignal(yCordpl1, sampleRate, label: "Ycord");
+new ScottPlot.FormsPlotViewer(plt).ShowDialog();
 
 
 /* foreach (var bruh in universe.planets[0].xPoslist)
